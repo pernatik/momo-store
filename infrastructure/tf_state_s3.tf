9e36d09619779a11ec8de10f7a3c6e55e9435c37
Andrey Kovalev <grafkovalev@yandex.ru>
2023-04-12 19:36:30 +0300

    Update tf_state_s3.tf

--- infrastructure/tf_state_s3.tf
+++ infrastructure/tf_state_s3.tf
@@ -6,8 +6,8 @@ terraform {
     bucket     = "momo-tf-state"
     region     = "ru-central1"
     key        = "terraform.tfstate"
-    access_key = "YCAJEbZKbqyhhmhK2c17ZW9-y"
-    secret_key = "YCPzFOyH-CknloVxfbj0frknzE-hiAdTGdHLp-lN"
+    access_key = ""
+    secret_key = ""
 
     skip_region_validation      = true
     skip_credentials_validation = true
